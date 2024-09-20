79e2d490a75b0825abab943100c5bce9d4cc80fe
Eliza <elbondarenko04@gmail.com>
2024-09-20 22:44:48 +0300

    works

--- app/src/main/java/com/example/kotlin2/MainActivity.kt
+++ app/src/main/java/com/example/kotlin2/MainActivity.kt
@@ -3,32 +3,35 @@ package com.example.kotlin2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.kotlin2.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonFirst.setOnClickListener{
+        fragmentManager = supportFragmentManager
+        binding.buttonFirst.setOnClickListener {
             replaysFragment(Fragment1())
 
         }
-        binding.buttonSecond.setOnClickListener{
+        binding.buttonSecond.setOnClickListener {
             replaysFragment(Fragment2())
 
         }
-        binding.buttonThird.setOnClickListener{
+        binding.buttonThird.setOnClickListener {
             replaysFragment(Fragment3())
 
         }
     }
-    private fun replaysFragment(fragment : Fragment) {
 
-    val fragmentManager = supportFragmentManager
+    private fun replaysFragment(fragment : Fragment) {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.commit()
